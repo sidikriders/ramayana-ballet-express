faa98e7144fcb8ab970179a55cae29ac2ca0ad28
sidikriders <sidik@xwork.id>
2018-03-29 14:03:52 +0700

    mulai lembaran baru, pindahin server ke root folder git

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var userRole = require('./routes/userRoles')
-var userRoute = require('./routes/user')
-
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/user-role', userRole)
-app.use('/user', userRoute)
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  err.data = 'Not Found'
   next(err);
 });
 
@@ -34,10 +24,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.send({
-    status: false,
-    data: err
-  });
+  res.send(err);
 });
 
 module.exports = app;
